f2a442a18a3d12c418559a77ed62c3d249175d45
Rikharthu <uberviolence@gmail.com>
2017-08-02 10:18:29 +0300

    eventbus

--- src/main/java/com/uberv/example/Main.java
+++ src/main/java/com/uberv/example/Main.java
@@ -3,30 +3,59 @@ package com.uberv.example;
 import io.reactivex.Observable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
+import org.reactivestreams.Subscriber;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
 
+    // TODO more info https://medium.com/@seventyone/a-simple-event-bus-with-rxjava-46cc1858df23
     public static void main(String[] args) throws InterruptedException {
         Random rnd = new Random();
 
-        MyEventBus eventsBus = new MyEventBus();
-        eventsBus.subscribe(new Consumer<MyEvent>() {
-            @Override
-            public void accept(MyEvent myEvent) throws Exception {
-                System.out.println("Received Event of type " + myEvent.getClass().getSimpleName());
-                if (myEvent instanceof OnNewIntegerEvent) {
-                    OnNewIntegerEvent onNewIntEvent = (OnNewIntegerEvent) myEvent;
-                    System.out.println("OnNewIntegerEvent : " + onNewIntEvent.getValue());
+        MyEventBus eventsBus = MyEventBus.instanceOf();
+//        eventsBus.subscribe(myEvent -> {
+//            System.out.println("Received Event of type " + myEvent.getClass().getSimpleName());
+//            if (myEvent instanceof OnNewIntegerEvent) {
+//                OnNewIntegerEvent onNewIntEvent = (OnNewIntegerEvent) myEvent;
+//                System.out.println("OnNewIntegerEvent : " + onNewIntEvent.getValue());
+//            }
+//        });
+//
+//        eventsBus.subscribe(myEvent -> {
+//            System.out.println("2) Received Event of type " + myEvent.getClass().getSimpleName());
+//            if (myEvent instanceof OnNewIntegerEvent) {
+//                OnNewIntegerEvent onNewIntEvent = (OnNewIntegerEvent) myEvent;
+//                System.out.println("2) OnNewIntegerEvent : " + onNewIntEvent.getValue());
+//            }
+//        });
+
+        Disposable floatSubscribtion=eventsBus
+                .observable(OnNewFloatEvent.class)
+                .subscribe(event -> {
+                    System.out.println("OnNewFloatEvent : " + event.getValue());
+                });
+
+        Disposable allSubscribtion=eventsBus
+                .observable(MyEvent.class)
+                .subscribe(event -> {
+                    System.out.println("OnNewXYZEvent : ");
+                });
+
+        new Thread(() -> {
+            while (true) {
+                int value = rnd.nextInt(100);
+                eventsBus.post(new OnNewIntegerEvent(value));
+                if(value>20 && value<50){
+                    eventsBus.postObject(new OnNewFloatEvent(rnd.nextFloat()));
+                }
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
-        });
-
-        while (true) {
-            eventsBus.post(new OnNewIntegerEvent(rnd.nextInt()));
-            Thread.sleep(300);
-        }
+        }).start();
     }
 }
--- src/main/java/com/uberv/example/MyEventBus.java
+++ src/main/java/com/uberv/example/MyEventBus.java
@@ -1,21 +1,47 @@
 package com.uberv.example;
 
+import com.sun.istack.internal.NotNull;
+import io.reactivex.Observable;
+import io.reactivex.Observer;
 import io.reactivex.functions.Consumer;
 import io.reactivex.subjects.PublishSubject;
 
 public class MyEventBus {
 
-    private PublishSubject<MyEvent> mEventPublisher;
+    private PublishSubject<Object> mEventPublisher;
+
+    public static MyEventBus sInstance;
+
+    public static MyEventBus instanceOf() {
+        if (sInstance == null) {
+            sInstance = new MyEventBus();
+        }
+        return sInstance;
+    }
+
 
     public MyEventBus() {
         this.mEventPublisher = PublishSubject.create();
     }
 
     public void subscribe(Consumer<MyEvent> eventConsumer) {
-        mEventPublisher.subscribe(eventConsumer);
+        mEventPublisher.subscribe((Observer<? super Object>) eventConsumer);
     }
 
     public void post(MyEvent event) {
         mEventPublisher.onNext(event);
+    }
+
+    public <T> Observable<T> observable(@NotNull Class<T> eventClass){
+        return this.mEventPublisher
+                .filter(o->o!=null)
+                .filter(eventClass::isInstance)
+                .cast(eventClass);
+    }
+
+    public void postObject(Object event) {
+        if (sInstance.mEventPublisher.hasObservers()) {
+            sInstance.mEventPublisher.onNext(event);
+        }
     }
 }
